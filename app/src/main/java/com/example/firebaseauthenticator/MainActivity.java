90c0f5d73d7cea973f42d2ef5af69a32197be326
Adeel <71775487+ade3l@users.noreply.github.com>
2021-06-03 13:01:22 +0300

    Sign in with google button added

--- app/src/main/java/com/example/firebaseauthenticator/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthenticator/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.firebaseauthenticator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignInButton signInButton=findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Sign in button clicked", Toast.LENGTH_SHORT).show());
     }
 }
